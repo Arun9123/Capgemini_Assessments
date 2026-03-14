a4ed09215ea6e6b6368960efd1786128663fba9d
Arunabha Dutta <143066490+Arun9123@users.noreply.github.com>
2026-03-15 01:10:45 +0530

    XML based configuration in Spring

---
+++ Weekly_Assessments/Weekly_Assessment_Mar14/spring1/src/main/java/com/capgemini/spring/App.java
@@ -0,0 +1,17 @@
+package com.capgemini.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean-config.xml");
+        
+        Student student = (Student)context.getBean("student");
+        System.out.println(student);
+        
+        Employee employee = (Employee)context.getBean("employee");
+        System.out.println(employee);
+        
+    }
+}
